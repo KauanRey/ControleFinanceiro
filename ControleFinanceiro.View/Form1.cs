a52a01e4026cd7d0a96f98b45ce57a31ad647b6e
KauanRey <kauanreycon934@gmail.com>
2022-07-29 08:46:05 -0300

    Criando um Controle Financeiro

---
+++ ControleFinanceiro.View/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleFinanceiro.View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            * 
+            * O que é uma variável?
+            * Tipos de variáveis 
+            * Declarar ou omitir o tipo var
+            * 
+            * var email = "ceci@gmail.com";
+            *  int senha = 123456;
+            */
+
+            string email, senha;
+            email = textBox1.Text;
+            senha = textBox2.Text;
+
+            MessageBox.Show($"Vó Cecilia seu email {email} esta correto?");
+
+
+            MessageBox.Show("Olá C#");
+        }
+    }
+}
